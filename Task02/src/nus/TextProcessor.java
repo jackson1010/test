c9329f6e4b86d54b60960205267e3ef5eb10b31e
jackson1010 <zinwin_1994@gmail.com>
2023-02-14 15:50:40 +0800

    updates

--- Task02/src/nus/TextProcessor.java
+++ Task02/src/nus/TextProcessor.java
@@ -11,8 +11,8 @@ public class TextProcessor {
 
     public static void main(String[] args) throws IOException {
         Map<String, Map<String, Integer>> bigMap = new HashMap<>();
-        Map<String, Integer> wordCount = new HashMap<>();
-        Map<String, Integer> currentCount =new HashMap<>();
+        Map<String, Integer> wordAndCount = new HashMap<>();
+        Map<String, Integer> currentCount = new HashMap<>();
 
         String p = args[0];
         File directory = new File(p);
@@ -24,21 +24,21 @@ public class TextProcessor {
                     FileReader fr = new FileReader(file);
                     BufferedReader br = new BufferedReader(fr);
                     String line;
-                    //br.readLine();
-                    System.out.println(file.getName());
-
+                    br.readLine();
                     while ((line = br.readLine()) != null) {
-                        String newLine = line.replaceAll("[^a-zA-Z0-9 ]", "");
-                        System.out.printf("> %s\n", newLine.toLowerCase());
-                        String[] words = newLine.toLowerCase().split(" ");
+                        if (!line.isEmpty()) {
+                            String newLine = line.replaceAll("[^a-zA-Z0-9 ]", "");
+                            String[] words = newLine.toLowerCase().trim().split(" ");
+                            //System.out.println(newLine);
 
-                        for (int i = 0; i < words.length - 1; i++) {
-                            String currentWord = words[i];
-                            String NextWord = words[i + 1];
-                            wordCount = bigMap.getOrDefault(currentWord, new HashMap<>());
-                            wordCount.put(NextWord, wordCount.getOrDefault(NextWord, 0) + 1);
-                            currentCount.put(currentWord,currentCount.getOrDefault(currentWord, 0)+1);
-                            bigMap.put(currentWord, wordCount);
+                            for (int i = 0; i < words.length - 1; i++) {
+                                String currentWord = words[i];
+                                String NextWord = words[i + 1];
+                                wordAndCount = bigMap.getOrDefault(currentWord, new HashMap<>());
+                                wordAndCount.put(NextWord, wordAndCount.getOrDefault(NextWord, 0) + 1);
+                                currentCount.put(currentWord, currentCount.getOrDefault(currentWord, 0) + 1);
+                                bigMap.put(currentWord, wordAndCount);
+                            }
                         }
                     }
                     br.close();
@@ -49,26 +49,19 @@ public class TextProcessor {
             }
         }
 
-
         for (String word : bigMap.keySet()) {
-            wordCount = bigMap.get(word);
-            int currentWordCount = currentCount.get(word);
-            System.out.print(word);
-            System.out.println();
-            for (String nextWord : wordCount.keySet()) {
-                int count = wordCount.get(nextWord);
-                double probability = (double) count/currentWordCount;
-                System.out.println("    " + nextWord + " " + probability);
-            }
+        wordAndCount = bigMap.get(word);
+        int currentWordCount = currentCount.get(word);
+        if(!word.isEmpty()){
+        System.out.print(word);
+        System.out.println();
+        }
+        for (String nextWord : wordAndCount.keySet()) {
+        int count = wordAndCount.get(nextWord);
+        double probability = (double) count/currentWordCount;
+        System.out.printf("    " + nextWord + " %.2f\n", probability);
+        }
         }
 
-    }
-
-    private static int getTotalLine(BufferedReader br) throws IOException {
-        int count = 0;
-        while (br.readLine() != null) {
-            count++;
-        }
-        return count++;
     }
 }
